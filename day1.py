0f2cb50bdd3efc843cf6670125a48d91ce9f270f
pragyanshu57 <42099048+pragyanshu57@users.noreply.github.com>
2020-12-03 08:30:27 -0800

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,40 @@
+'''
+print{'hello sarthak'}
+a=3
+b=4
+print a,b
+a,b=b,a
+print a,b
+
+a=raw_input ('enter string : ')
+print a
+print type(a)
+
+a=input('enter : ')
+print a
+print type(a)
+
+a=raw_input ('enter string : ')
+b=int(a)
+print b+6
+print type(b)
+
+a=int(raw_input ('enter string : '))
+print a+5
+
+
+a=float(raw_input ('enter string : '))
+print a+5
+
+a,b,c=4,2,5
+print 'largest is : '
+if(a>b&a>c):
+    print a
+elif(b>a&b>c):
+    print b
+elif(c>a&c>b):
+    print c
+else:
+    print 'wrong input'
+'''
+
